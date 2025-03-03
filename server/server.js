4edeb7698c6f28874eb785a76cea21e72aaab00e
Naoto Abe <naotoabe1@gmail.com>
2025-03-03 23:00:26 +0100

    revert back endpoint

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 // Azure AI configuration
-const endpoint = "https://ai-supportdeepseekai250024594703.openai.azure.com/" //"https://ai-supportdeepseekai250024594703.services.ai.azure.com/models";
+const endpoint = "https://ai-supportdeepseekai250024594703.services.ai.azure.com/models";
 const deploymentId = "gpt-4o-mini"; //"DeepSeek-R1-statstream"; 
 const clientOptions = { 
   credentials: {
